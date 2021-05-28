ee07989f8fb470cda27a5b242858959085fcd544
Nancy <liveislife1997@gmail.com>
2021-05-28 15:35:31 -0500

    cambiamos el puerto

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+
+const port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + "/public/"));
 app.listen('3000', function(){
     console.log("Servidor web iniciado en puerto 3000. http://localhost:3000//");
