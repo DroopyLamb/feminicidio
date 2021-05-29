c845216bf4c0f253f5d94945e604c349ce5bf393
unknown <csaulhiram@outlook.com>
2021-05-28 20:08:09 -0500

    mi primer commit

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+
 app.use(express.static(__dirname + "/public/"));
-app.listen('3000', function(){
-    console.log("Servidor web iniciado en puerto 3000. http://localhost:3000//");
+app.listen('3000', function() {
+    console.log("Servidor web iniciado en puerto 3000. https://localhost:3000//");
 })
